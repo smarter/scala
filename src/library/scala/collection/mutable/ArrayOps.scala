3c8fe1236249dbaad4c788e8b863f93f3a2f527a
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-03-30 09:23:17 +0200

    Minor stdlib changes needed for the tuple PR
    - scala/collection/mutable/ArrayOps.scala
    
    Because _i are added via implicit conversion, these lines now need to trigger two implicit conversions in a raw, which is not supported by scalac.
    
    - scala/math/Ordering.scala
    
    Somehow for the second argument of the `Tuple5` does not uses the (...) syntax. Because of the scala package it references scala.Tuple5, which means the method is not a proper override for def compare.

--- src/library/scala/collection/mutable/ArrayOps.scala
+++ src/library/scala/collection/mutable/ArrayOps.scala
@@ -136,8 +136,8 @@ sealed trait ArrayOps[T] extends Any with ArrayLike[T, Array[T]] with CustomPara
     var i = 0
     while (i < length) {
       val e = apply(i)
-      a1(i) = e._1
-      a2(i) = e._2
+      a1(i) = asPair(e)._1
+      a2(i) = asPair(e)._2
       i += 1
     }
     (a1, a2)
@@ -169,9 +169,9 @@ sealed trait ArrayOps[T] extends Any with ArrayLike[T, Array[T]] with CustomPara
     var i = 0
     while (i < length) {
       val e = apply(i)
-      a1(i) = e._1
-      a2(i) = e._2
-      a3(i) = e._3
+      a1(i) = asTriple(e)._1
+      a2(i) = asTriple(e)._2
+      a3(i) = asTriple(e)._3
       i += 1
     }
     (a1, a2, a3)
